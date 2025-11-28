48fe25dc6b16310e8705a8f685d7bc23a87cb775
0leum <ritam.d3v@gmail.com>
2025-11-28 22:48:58 +0530

    hosting variable injection

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -11,26 +11,11 @@ type FirebaseSdks = {
   firestore: Firestore;
 }
 
-// IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase(): FirebaseSdks {
   if (!getApps().length) {
-    // Important! initializeApp() is called without any arguments because Firebase App Hosting
-    // integrates with the initializeApp() function to provide the environment variables needed to
-    // populate the FirebaseOptions in production. It is critical that we attempt to call initializeApp()
-    // without arguments.
-    let firebaseApp;
-    try {
-      // Attempt to initialize via Firebase App Hosting environment variables
-      firebaseApp = initializeApp();
-    } catch (e) {
-      // Only warn in production because it's normal to use the firebaseConfig to initialize
-      // during development
-      if (process.env.NODE_ENV === "production") {
-        console.warn('Automatic initialization failed. Falling back to firebase config object.', e);
-      }
-      firebaseApp = initializeApp(firebaseConfig);
-    }
-
+    // When deploying to any environment other than Firebase App Hosting,
+    // we need to explicitly provide the configuration.
+    const firebaseApp = initializeApp(firebaseConfig);
     return getSdks(firebaseApp);
   }
 
